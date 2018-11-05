f7568a0230a3793fececf2c91bf571b2ffcd00d9
Chris Cooksley <christopher.cooksley@gmail.com>
2018-11-05 11:51:42 +0000

    updated spec_helper to use SimpleCov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,17 +1,17 @@
 ENV['ENVIRONMENT'] = 'test'
-require File.join(File.dirname(__FILE__), '..', 'app.rb')
 require 'capybara'
 require 'capybara/rspec'
 require 'rspec'
-
+require_relative './../app'
 
 Capybara.app = MakersBNB
 
-RSpec.configure do |config|
-  config.before(:each) do
-    setup_test_database
-  end
-end
+SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
+  SimpleCov::Formatter::Console,
+  # Want a nice code coverage website? Uncomment this next line!
+  # SimpleCov::Formatter::HTMLFormatter
+])
+SimpleCov.start
 
 RSpec.configure do |config|
   config.include Capybara::DSL
