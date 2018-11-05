e81de7c1bb401a7aca64c0fb3cfd438bf5be447b
rbbri <robin.s.johnson@gmail.com>
2018-11-05 16:24:33 +0000

    Added a feature test for listing a space

---
+++ spec/features/listing_spec.rb
@@ -0,0 +1,10 @@
+feature 'listing a space' do
+  scenario 'a logged in user lists a new space' do
+    visit '/spaces'
+    click_button 'List a Space'
+    fill_in 'name', with: 'Listing 1'
+    click_button 'List my Space'
+    expect(page).to have_content('Listing 1')
+
+  end
+end
