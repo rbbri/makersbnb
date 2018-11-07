5bde5db90e0a783c6dfa76850ad824b95ca0782b
Chris Cooksley <christopher.cooksley@gmail.com>
2018-11-07 10:46:38 +0000

    navbar for requests

--- spec/features/helper_methods.rb
+++ spec/features/helper_methods.rb
@@ -14,6 +14,14 @@ def signup2
   click_button 'Sign Up'
 end
 
+def signup3
+  fill_in(:username, with: 'user3')
+  fill_in(:password, with: 'password3')
+  fill_in(:name, with: 'User Three')
+  fill_in(:email, with: 'user3@example.com')
+  click_button 'Sign Up'
+end
+
 def signup_duplicate_user
   fill_in(:username, with: 'user1')
   fill_in(:password, with: 'password1')
@@ -35,4 +43,24 @@ def signin1
   fill_in(:username, with: 'user1')
   fill_in(:password, with: 'password1')
   click_button 'log_in'
+end
+
+def list_cornwall
+  click_button 'List a Space'
+  fill_in(:name, with: 'Beautiful house in Cornwall')
+  fill_in(:description, with: 'This is a beautiful house in Cornwall')
+  fill_in(:price, with: '100')
+  click_button 'List my Space'
+end
+
+def list_devon
+  click_button 'List a Space'
+  fill_in(:name, with: 'Beautiful house in Devon')
+  fill_in(:description, with: 'This is a beautiful house in Devon')
+  fill_in(:price, with: '100')
+  click_button 'List my Space'
+end
+
+def request_cornwall
+
 end
--- spec/features/requests_spec.rb
+++ spec/features/requests_spec.rb
@@ -3,14 +3,16 @@ feature 'requesting a space' do
     visit '/'
     signup1
     click_button 'List a Space'
-    fill_in(:name, with: 'Beautiful house in cornwall')
+    fill_in(:name, with: 'Beautiful house in Cornwall')
     fill_in(:description, with: 'This is a beautiful house in Cornwall')
     fill_in(:price, with: '100')
     click_button 'List my Space'
     click_button 'Logout'
     signup2
-    click_link('Beautiful house in cornwall')
+    click_link('Beautiful house in Cornwall')
     click_button('Request to Book')
-    expect(page).to have_content('Beautiful house in cornwall')
+    click_button 'Logout'
+    signup3
+    expect(page).to have_content('Beautiful house in Cornwall')
   end
 end
