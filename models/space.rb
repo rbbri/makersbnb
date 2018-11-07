dd3163725825f0def419725b89c880680002f2c1
Chris Cooksley <christopher.cooksley@gmail.com>
2018-11-07 10:35:59 +0000

    requests spec fills in all fields; space model requires all fields

--- models/space.rb
+++ models/space.rb
@@ -3,7 +3,7 @@ class Space < ActiveRecord::Base
   belongs_to :user, foreign_key: 'user_id'
   has_many :requests
   validates :name, presence: true
-  # validates :description, presence: true
-  # validates :price, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
   validates :user_id, presence: true, uniqueness: true
 end
--- spec/features/requests_spec.rb
+++ spec/features/requests_spec.rb
@@ -4,6 +4,8 @@ feature 'requesting a space' do
     signup1
     click_button 'List a Space'
     fill_in(:name, with: 'Beautiful house in cornwall')
+    fill_in(:description, with: 'This is a beautiful house in Cornwall')
+    fill_in(:price, with: '100')
     click_button 'List my Space'
     click_button 'Logout'
     signup2
