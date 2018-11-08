391f97127f136189f9c9feacb15bfea39d86e0d1
Chris Cooksley <christopher.cooksley@gmail.com>
2018-11-08 14:59:00 +0000

    removes p params from app

--- spec/features/helper_methods.rb
+++ spec/features/helper_methods.rb
@@ -71,13 +71,15 @@ end
 
 def request_cornwall
   click_link 'Cornwall'
+  fill_in("start_date", with: "2018-11-11")
+  select '1', :from => 'nights'
   click_button 'Request to Book'
 end
 
 def request_devon
   click_link 'Devon'
-  fill_in("start_date", with: "2018-11-5")
-  select '4', :from => 'nights'
+  fill_in("start_date", with: "2018-11-12")
+  select '2', :from => 'nights'
   click_button 'Request to Book'
 end
 
